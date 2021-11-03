6d135875d7f3e7424e850e9d7f1361fadfa54199
Swiftcure <mtm05040@gmail.com>
2021-11-03 20:08:56 +0100

    Bulls and cows script and readme info

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # PY_training
-Tasks for python training and projects.
+Repository for small python projects and training.
 
 ### text_analyzer.py
-Short script with 3 predefined articles, which will be numerically analyzed. Check the code for usernames, passwords and user options.
+Short script with 3 predefined articles, which will be numerically analyzed. 
+Check the code for usernames, passwords and user options.
+
+### bulls_and_cows.py
+Another short script based on the well known number guessing game Bulls & Cows.
--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -2,67 +2,103 @@
 author = Michal Tomasek
 '''
 
-import getpass
 import random
-from time import sleep
+from math import floor
+from time import sleep, time
 
 sepa = "░▒▓█▓▒" * 9 + "░"
 
-#print(sepa)
-#logo = """
-#
-#██████╗ ██╗   ██╗██╗     ██╗     ███████╗
-#██╔══██╗██║   ██║██║     ██║     ██╔════╝
-#██████╔╝██║   ██║██║     ██║     ███████╗
-#██╔══██╗██║   ██║██║     ██║     ╚════██║
-#██████╔╝╚██████╔╝███████╗███████╗███████║
-#╚═════╝  ╚═════╝ ╚══════╝╚══════╝╚══════╝
-#
-# █████╗ ███╗   ██╗██████╗
-#██╔══██╗████╗  ██║██╔══██╗
-#███████║██╔██╗ ██║██║  ██║
-#██╔══██║██║╚██╗██║██║  ██║
-#██║  ██║██║ ╚████║██████╔╝
-#╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝
-#
-# ██████╗ ██████╗ ██╗    ██╗███████╗
-#██╔════╝██╔═══██╗██║    ██║██╔════╝
-#██║     ██║   ██║██║ █╗ ██║███████╗
-#██║     ██║   ██║██║███╗██║╚════██║
-#╚██████╗╚██████╔╝╚███╔███╔╝███████║
-# ╚═════╝ ╚═════╝  ╚══╝╚══╝ ╚══════╝
-#"""
-#
-#for line in logo.split("\n"):
-#    print(line)
-#    sleep(0.5)
-#print(sepa)
-#print("WELCOME TO THE ULTIMATE GAME OF BULLS AND COWS!\nFour digit number is ready for you now!\nRemember, there are no duplicate digits in the number\nand the number can't start with zero.")
-#print(sepa)
+print(sepa)
+logo = """
 
-#repeat = True
-#while repeat:
-#    number = random.choice(range(1000, 10000))
-#    for digit in str(number):
-#        if str(number).count(digit) > 1:
-#            repeat = True
-#    repeat = False
+ ██████╗ ██╗   ██╗██╗     ██╗     ███████╗
+ ██╔══██╗██║   ██║██║     ██║     ██╔════╝
+ ██████╔╝██║   ██║██║     ██║     ███████╗
+ ██╔══██╗██║   ██║██║     ██║     ╚════██║
+ ██████╔╝╚██████╔╝███████╗███████╗███████║
+ ╚═════╝  ╚═════╝ ╚══════╝╚══════╝╚══════╝
 
-while True:
-    number = str(random.choice(range(1000, 10000)))
-    if len(set(number)) == 4:
-        break
+  █████╗ ███╗   ██╗██████╗
+ ██╔══██╗████╗  ██║██╔══██╗
+ ███████║██╔██╗ ██║██║  ██║
+ ██╔══██║██║╚██╗██║██║  ██║
+ ██║  ██║██║ ╚████║██████╔╝
+ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝
 
-print(number)
+  ██████╗ ██████╗ ██╗    ██╗███████╗
+ ██╔════╝██╔═══██╗██║    ██║██╔════╝
+ ██║     ██║   ██║██║ █╗ ██║███████╗
+ ██║     ██║   ██║██║███╗██║╚════██║
+ ╚██████╗╚██████╔╝╚███╔███╔╝███████║
+  ╚═════╝ ╚═════╝  ╚══╝╚══╝ ╚══════╝
+"""
 
-while True:
-    guess = input("Enter a number: ")
-    if guess.isnumeric() and len(guess) == 4 and guess[0] != "0" and len(set(guess)) == 4:
-        break
-    else:
-        print("Four... digit-unique... number... not starting with zero. It's not so difficult.")
+for line in logo.split("\n"):
+    print(line)
+    sleep(0.4)
+print(sepa)
+print(" WELCOME TO THE ULTIMATE GAME OF BULLS AND COWS!\n Four digit number is ready for you now!\n Remember, there are no duplicate digits in the number\n and the number can't start with zero.\n Now get to it, your time's running!")
+print(sepa)
 
-print(guess)
+repeat = True         #This is a second (worse) option for the while loop below.
+while repeat:
+    number = random.choice(range(1000, 10000))
+    for digit in str(number):
+        if str(number).count(digit) > 1:
+            repeat = True
+    repeat = False
+
+def num_valid_check(guess):
+    while True:
+        if guess.isnumeric() and guess[0] != "0" and len(set(guess)) == 4:
+            return 1
+        else:
+            print(" Four... digit-unique... number... not starting with zero. It's not so difficult.")
+            return 0
+
+def num_generator():
+    while True:
+        number = str(random.choice(range(1000, 10000)))
+        if len(set(number)) == 4:
+            return number
+
+number = num_generator()
+bull, cow = 0, 0
+total_bull, total_cow = 0, 0
+attempts = 0
+start = time()
+
+while True:                                          #Main loop for the game
+    guess = input("\n Enter a number: ")
+
+    if num_valid_check(guess) == False:
+        continue
+
+    for n in guess:
+        if n == number[guess.index(n)]:
+            bull += 1
+        elif n != number[guess.index(n)] and n in number:
+            cow += 1
+
+    print(f" BULLS: {bull}\n COWS: {cow}")
+
+    attempts += 1
+    total_bull += bull
+    total_cow += cow
+
+    if bull == 4:
+        end = time()
+        total_time = end - start
+        print(f" HOLY MOLLY! YOU DID IT! \n It took 'only' {attempts} attempts and {floor(total_time/60)} min and {floor(total_time%60)} sec.\n You have collected {total_bull} bulls and {total_cow} cows in total. That's quite a herd.")
+        if input(f" Wanna go again? (y/n) ") != "y":
+            print(" Thank you for playing! You still have a lot to learn though...")
+            sleep(5)
+            exit()
+        else:
+            print(sepa)
+            number = num_generator()
+
+    bull, cow = 0, 0
 
 
 
